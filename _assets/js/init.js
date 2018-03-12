393d162eec5fcc5017c524ff6141d81be8e81540
Angus Thompson <angustho@gmail.com>
2018-03-12 14:38:29 +1000

    Added "categories" and updated Jekyll

--- _assets/js/init.js
+++ _assets/js/init.js
@@ -79,43 +79,6 @@ $(window).on('scroll', { previousTop: 0 }, function() {
 		 }
 	 });
 
-/* ==================================================
-	  Slider Settings
-================================================== */
-function ago(date) {
-    function render(n, unit) {
-        return n + " " + unit + ((n == 1) ? "" : "s") + " ago";
-    }
-
-    var seconds = Math.floor((new Date() - date) / 1000);
-
-    var interval = Math.floor(seconds / (60 * 60 * 24 * 30 * 365));
-    if (Math.floor(seconds / (60 * 60 * 24 * 30 * 365)) >= 1) {
-        return render(interval, "year");
-    }
-    interval = Math.floor(seconds / (60 * 60 * 24 * 30));
-    if (interval >= 1) {
-        return render(interval, "month");
-    }
-    interval = Math.floor(seconds / (60 * 60 * 24));
-    if (interval >= 1) {
-        return render(interval, "day");
-    }
-    interval = Math.floor(seconds / (60 * 60));
-    if (interval >= 1) {
-        return render(interval, "hour");
-    }
-    interval = Math.floor(seconds / 60);
-    if (interval >= 1) {
-        return render(interval, "minute");
-    }
-    interval = Math.floor(seconds);
-    return render(interval, "second");
-}
-
-var date = Date.parse($('.post-meta > .posted').attr('datetime'));
-$('.post-meta > .posted').html(ago(date));
-
 
 /* ==================================================
     Slider Settings
